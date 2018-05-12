4ec5bfa758a6227435ae53fa0d5a4a1c9a9c7bc5
KAHA <wannasaynone@gmail.com>
2018-05-12 22:22:06 +0800

    Import animations, actor controller

---
+++ Assets/ActorController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour {
+
+    private const string ANIMATOR_PARA_NAME_FORWARD = "forward";
+    private const string ANIMATOR_PARA_NAME_JUMP = "jump";
+
+    [SerializeField] private InputModule m_input = null;
+    [SerializeField] private Animator m_modelAnimator = null;
+    [SerializeField] private Rigidbody m_rigidbody = null;
+    [SerializeField] private float m_rotateSpeed = 10f;
+    [SerializeField] private float m_moveSpeed = 1.5f;
+    [SerializeField] private float m_runScale = 2.5f;
+
+    private Vector3 m_movingVector = Vector3.zero;
+    private Transform m_model = null;
+
+    private void Awake()
+    {
+        m_model = m_modelAnimator.transform;
+    }
+
+    private void Update ()
+    {
+        SetMoveMotion();
+    }
+
+    private void FixedUpdate()
+    {
+        m_rigidbody.velocity = new Vector3(m_movingVector.x, m_rigidbody.velocity.y, m_movingVector.z);
+    }
+
+    private void SetMoveMotion()
+    {
+        m_modelAnimator.SetFloat(ANIMATOR_PARA_NAME_FORWARD, m_input.Direction_MotionCurveValue);
+        if(m_input.Jump)
+        {
+            m_modelAnimator.SetTrigger(ANIMATOR_PARA_NAME_JUMP);
+        }
+        m_movingVector = m_input.Direction_MotionCurveValue * m_model.forward * m_moveSpeed * (m_input.Running ? m_runScale  : 1f);
+        RotateModel();
+    }
+
+    private void RotateModel()
+    {
+        // 避免在水平值和垂直值過低時重設模型為"正面"
+        if(Mathf.Abs(m_input.Direction_Horizontal) <= 0.1 && Mathf.Abs(m_input.Direction_Vertical) <= 0.1)
+        {
+            return;
+        }
+        else
+        {
+            m_model.forward = Vector3.Slerp(m_model.forward, m_input.Direction_Vector, m_rotateSpeed);
+        }
+    }
+
+}
--- Assets/InputModule.cs
+++ Assets/InputModule.cs
@@ -5,20 +5,42 @@ using UnityEngine;
 public class InputModule : MonoBehaviour {
 
     private static bool INPUT_ENABLE = true;
+    private const float MOVE_MOTION_SACLE = 1f;
+    private const float RUN_MOTION_SCALE = 2f;
 
-	[SerializeField] private string m_keyUp = "w";
+    [SerializeField] private string m_keyUp = "w";
 	[SerializeField] private string m_keyDown = "s";
 	[SerializeField] private string m_keyRight = "d";
 	[SerializeField] private string m_keyLeft = "a";
+    [SerializeField] private string m_keyA = "left shift";
+    [SerializeField] private string m_keyB = "space";
+    [SerializeField] private string m_keyC = "q";
+    [SerializeField] private string m_keyD = "r";
     [SerializeField] private float m_moveSmoothTime = 0.1f;
 
-	private float m_direction_Vertical = 0f;
-	private float m_direction_Horizontal = 0f;
+    public float Direction_Vertical { get { return m_direction_vertical; } }
+    public float Direction_Horizontal { get { return m_direction_horizontal; } }
+    public float Direction_MotionCurveValue { get { return m_direction_motionCurveValue; } }
+    public Vector3 Direction_Vector { get { return m_direction_vector; } }
+    public bool Running { get { return m_isRun; } }
+    public bool Jump { get { return m_isJump;} }
 
-    private float m_target_direction_Vertical = 0f;
-    private float m_target_direction_Horizontal = 0f;
-    private float m_velocity_direction_Vertical = 0f;
-    private float m_velocity_direction_Horizontal = 0f;
+    private float m_direction_vertical = 0f;
+	private float m_direction_horizontal = 0f;
+
+    private float m_target_direction_vertical = 0f;
+    private float m_target_direction_horizontal = 0f;
+    private float m_velocity_direction_vertical = 0f;
+    private float m_velocity_direction_horizontal = 0f;
+
+    private float m_direction_motionCurveValue = 0f;
+    private Vector3 m_direction_vector = Vector3.zero;
+
+    private float m_target_motionCurveValue = 0f;
+    private bool m_isRun = false;
+
+    private bool m_isJump = false;
+    private bool m_lastJumpState = false;
 
     private void Update()
 	{
@@ -30,24 +52,64 @@ public class InputModule : MonoBehaviour {
         // 未來有可能會根據情況，讓玩家不能輸入，但還是要做角色移動(EX 過場)
         if (INPUT_ENABLE)
         {
-            float Vertical = (Input.GetKey(m_keyUp) ? 1f : 0f) - (Input.GetKey(m_keyDown) ? 1f : 0f);
-            float Horizontal = (Input.GetKey(m_keyRight) ? 1f : 0f) - (Input.GetKey(m_keyLeft) ? 1f : 0f);
-            SetDirection(Vertical, Horizontal);
+            float vertical = (Input.GetKey(m_keyUp) ? 1f : 0f) - (Input.GetKey(m_keyDown) ? 1f : 0f);
+            float horizontal = (Input.GetKey(m_keyRight) ? 1f : 0f) - (Input.GetKey(m_keyLeft) ? 1f : 0f);
+
+            m_isRun = Input.GetKey(m_keyA);
+
+            bool pressJump = Input.GetKey(m_keyB);
+            if (pressJump != m_lastJumpState)
+            {
+                m_isJump = true;
+            }
+            else
+            {
+                m_isJump = false;
+            }
+            m_lastJumpState = pressJump;
+
+            SetDirection(vertical, horizontal);
         }
     }
 
     private void SetDirection(float vertical, float horizontal)
     {
         // "前後左右"會根據狀況改變，所以不做"位移"(Vector3.forward * speed...之類的)，先做玩家輸入的方向判斷
-        vertical = Mathf.Clamp(vertical, -1f, 1f);
         horizontal = Mathf.Clamp(horizontal, -1f, 1f);
+        vertical = Mathf.Clamp(vertical, -1f, 1f);
 
-        m_target_direction_Vertical = vertical;
-        m_target_direction_Horizontal = horizontal;
+        // 先做轉換
+        Vector2 target_direction_InCircle = Direction_SquareViewToCircleView(new Vector2(horizontal, vertical));
+        float target_direction_InCircle_horizontal = target_direction_InCircle.x;
+        float target_direction_InCircle_vertical = target_direction_InCircle.y;
+
+        // 才做Run Scale放大縮小
+        m_target_direction_horizontal = target_direction_InCircle_horizontal * (m_isRun ? RUN_MOTION_SCALE : MOVE_MOTION_SACLE);
+        m_target_direction_vertical = target_direction_InCircle_vertical * (m_isRun ? RUN_MOTION_SCALE : MOVE_MOTION_SACLE);
 
         // 避免瞬間變值導致詭異的角色移動表現，用SmoothDamp製造類似遞增遞減的效果
-        m_direction_Vertical = Mathf.SmoothDamp(m_direction_Vertical, m_target_direction_Vertical, ref m_velocity_direction_Vertical, m_moveSmoothTime);
-        m_direction_Horizontal = Mathf.SmoothDamp(m_direction_Horizontal, m_target_direction_Horizontal, ref m_velocity_direction_Horizontal, m_moveSmoothTime);
+        m_direction_horizontal = Mathf.SmoothDamp(m_direction_horizontal, m_target_direction_horizontal, ref m_velocity_direction_horizontal, m_moveSmoothTime);
+        m_direction_vertical = Mathf.SmoothDamp(m_direction_vertical, m_target_direction_vertical, ref m_velocity_direction_vertical, m_moveSmoothTime);
+
+        // 用現在的水平值跟垂直值取得目前動作動畫的變化變量
+        m_direction_motionCurveValue = Mathf.Sqrt((m_direction_vertical * m_direction_vertical) + (m_direction_horizontal * m_direction_horizontal));
+
+        // 給予目前正在移動的方向向量
+        m_direction_vector = (m_direction_horizontal * transform.right + m_direction_vertical * transform.forward);
+    }
+
+    // https://arxiv.org/ftp/arxiv/papers/1509/1509.06344.pdf
+    private Vector2 Direction_SquareViewToCircleView(Vector2 input)
+    {
+        Vector2 output = Vector2.zero;
+
+        input.x = Mathf.Round(input.x);
+        input.y = Mathf.Round(input.y);
+
+        output.x = input.x * Mathf.Sqrt(1 - (input.y * input.y) / 2f);
+        output.y = input.y * Mathf.Sqrt(1 - (input.x * input.x) / 2f);
+
+        return output;
     }
 
 }
