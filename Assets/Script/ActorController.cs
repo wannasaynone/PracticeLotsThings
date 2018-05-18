2c7e13ab2354ae646a4c3f1871f3b6b56b499113
KAHA <wannasaynone@gmail.com>
2018-05-18 22:27:59 +0800

    use animator state machine behavior to control ActorController's state

--- Assets/Script/ActorController.cs
+++ Assets/Script/ActorController.cs
@@ -52,8 +52,6 @@ public class ActorController : MonoBehaviour {
     private AttackState m_currentAttack = AttackState.None;
     private MoveState m_currentMove = MoveState.None;
     private bool m_run = false;
-    private bool m_jump = false;
-    private bool m_attack = false;
 
     private Vector3 m_thrustVector3 = Vector3.zero;
     private Vector3 m_movingVector = Vector3.zero;
@@ -64,46 +62,150 @@ public class ActorController : MonoBehaviour {
     private void Awake()
     {
         m_model = m_modelAnimator.transform;
+
+        AnimatorEventSender.RegistOnStateEntered("ground", OnGroundEnter);
+        AnimatorEventSender.RegistOnStateEntered("jump", OnJumpEnter);
+        AnimatorEventSender.RegistOnStateEntered("fall", OnFallEnter);
+        AnimatorEventSender.RegistOnStateEntered("roll", OnRollEnter);
+        AnimatorEventSender.RegistOnStateEntered("jab", OnJabEnter);
+
+        AnimatorEventSender.RegistOnStateExited("ground", OnGroundExit);
+        AnimatorEventSender.RegistOnStateExited("jump", OnJumpExit);
+        AnimatorEventSender.RegistOnStateExited("fall", OnFallExit);
+        AnimatorEventSender.RegistOnStateExited("roll", OnRollExit);
+        AnimatorEventSender.RegistOnStateExited("jab", OnJabExit);
+
+        AnimatorEventSender.RegistOnStateUpdated("ground", OnGroundUpdate);
+        AnimatorEventSender.RegistOnStateUpdated("jump", OnJumpUpdate);
+        AnimatorEventSender.RegistOnStateUpdated("fall", OnFallUpdate);
+        AnimatorEventSender.RegistOnStateUpdated("roll", OnRollUpdate);
+        AnimatorEventSender.RegistOnStateUpdated("jab", OnJabUpdate);
     }
 
     private void Update ()
     {
-        ParseInputSingle();
+        ParseInputSignal();
+        ParseMotionSingal();
         DectectCollision(); 
     }
 
     private void FixedUpdate()
     {
-        SetVelocity();
+
+    }
+
+    private void OnGroundEnter(AnimatorEventArgs e)
+    {
+        Debug.Log("OnGroundEnter");
+    }
+
+    private void OnJumpEnter(AnimatorEventArgs e)
+    {
+        Debug.Log("OnJumpEnter");
+    }
+
+    private void OnFallEnter(AnimatorEventArgs e)
+    {
+        Debug.Log("OnFallEnter");
+    }
+
+    private void OnRollEnter(AnimatorEventArgs e)
+    {
+        Debug.Log("OnRollEnter");
+    }
+
+    private void OnJabEnter(AnimatorEventArgs e)
+    {
+        Debug.Log("OnJabEnter");
+    }
+
+    private void OnGroundExit(AnimatorEventArgs e)
+    {
+        Debug.Log("OnGroundExit");
+    }
+
+    private void OnJumpExit(AnimatorEventArgs e)
+    {
+        Debug.Log("OnJumpExit");
+    }
+
+    private void OnFallExit(AnimatorEventArgs e)
+    {
+        Debug.Log("OnFallExit");
+    }
+
+    private void OnRollExit(AnimatorEventArgs e)
+    {
+        Debug.Log("OnRollExit");
+    }
+
+    private void OnJabExit(AnimatorEventArgs e)
+    {
+        Debug.Log("OnJabExit");
+    }
+
+    private void OnGroundUpdate(AnimatorEventArgs e)
+    {
+        Debug.Log("OnGroundUpdate");
+    }
+
+    private void OnJumpUpdate(AnimatorEventArgs e)
+    {
+        Debug.Log("OnJumpUpdate");
+    }
+
+    private void OnFallUpdate(AnimatorEventArgs e)
+    {
+        Debug.Log("OnFallUpdate");
+    }
+
+    private void OnRollUpdate(AnimatorEventArgs e)
+    {
+        Debug.Log("OnRollUpdate");
+    }
+
+    private void OnJabUpdate(AnimatorEventArgs e)
+    {
+        Debug.Log("OnJabUpdate");
     }
 
     /////////////////////////////////
 
-    private void ParseInputSingle()
+    private void ParseInputSignal()
     {
-        m_attack = m_input.KeyCPressed;
-        m_jump = m_input.KeyBPressed;
         m_run = m_input.KeyAPressing;
-    }
 
-    private void SetVelocity()
-    {
-		if(!m_lockUpdateInputVelocity)
+        if (m_input.KeyCPressed)
         {
-            m_rigidbody.velocity = new Vector3(m_movingVector.x, m_rigidbody.velocity.y, m_movingVector.z) + m_thrustVector3;
+            m_modelAnimator.SetTrigger(ANIMATOR_PARA_NAME_ATTACK);
         }
 
-        m_thrustVector3 = Vector3.zero;
-    }
+        if(m_input.KeyBPressed)
+        {
+            m_modelAnimator.SetTrigger(ANIMATOR_PARA_NAME_JUMP);
+        }
 
-    private void DetectMove()
-    {
-        RotateModel();
         m_modelAnimator.SetFloat(ANIMATOR_PARA_NAME_FORWARD, m_input.Direction_MotionCurveValue);
-        m_movingVector = m_input.Direction_MotionCurveValue * m_model.forward * m_moveSpeed * (m_run ? m_runScale : 1f);
     }
 
-	private bool IsAnimatorInState(string stateName, string layerName = ANIMATOR_LAYER_NAME_0)
+    private void ParseMotionSingal()
+    {
+        m_movingVector = m_input.Direction_MotionCurveValue * m_model.forward * m_moveSpeed * (m_run ? m_runScale : 1f);
+        m_rigidbody.velocity = new Vector3(m_movingVector.x, m_rigidbody.velocity.y, m_movingVector.z) + m_thrustVector3;
+        RotateModel();
+    }
+
+    private bool IsJumping()
+    {
+        return m_rigidbody.velocity.y > 0.01f || m_rigidbody.velocity.y < -0.01f;
+    }
+
+    private bool IsIdle()
+    {
+        return m_input.Direction_MotionCurveValue <= 0.1f;
+    }
+
+    private bool IsAnimatorInState(string stateName, string layerName = ANIMATOR_LAYER_NAME_0)
 	{
 		return m_modelAnimator.GetCurrentAnimatorStateInfo(m_modelAnimator.GetLayerIndex(layerName)).IsName(stateName);
 	}
@@ -111,6 +213,7 @@ public class ActorController : MonoBehaviour {
     private void DectectCollision()
     {
         IsGrounded = CollisionDetector.DectectCollision(m_capcaol, LAYER_MASK_NAME_GROUND, m_adjustCollision);
+        m_modelAnimator.SetBool(ANIMATOR_PARA_NAME_IS_GROUND, IsGrounded);
     }
 
     private void RotateModel()
